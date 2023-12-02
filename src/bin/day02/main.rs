b606abc3189ef51191a98b118e82c0b1e16c3bc4
Benjamin <f.benjamin.larsson@gmail.com>
2023-12-02 12:24:28 +0100

    Day 2 part b

--- src/bin/day02/main.rs
+++ src/bin/day02/main.rs
@@ -1,8 +1,8 @@
-use std::{env, time::Instant};
+use std::{env, time::Instant, cmp::max};
 
 const DAY: u32 = 2;
 
-fn are_balls_possible(count: u32, color: &str) -> bool {
+fn are_cubes_possible(count: u32, color: &str) -> bool {
     match color {
         "red" => count <= 12,
         "green" => count <= 13,
@@ -13,16 +13,13 @@ fn are_balls_possible(count: u32, color: &str) -> bool {
 
 fn verify_game(line: &str) -> bool {
     let results = line.split(':').skip(1).next().unwrap();
-    let rounds = results.split(';').map(|round| round.trim());
-    for round in rounds {
-        let balls = round.split(",").map(|b| b.trim());
-        for ball in balls {
-            let mut split = ball.split(' ');
-            let count: u32 = split.next().unwrap().parse().ok().unwrap();
-            let color = split.next().unwrap();
-            if !are_balls_possible(count, color) {
-                return false;
-            }
+    let cubes = results.split([';', ',']).map(|cube| cube.trim());
+    for cube in cubes {
+        let mut split = cube.split(' ');
+        let count: u32 = split.next().unwrap().parse().ok().unwrap();
+        let color = split.next().unwrap();
+        if !are_cubes_possible(count, color) {
+            return false;
         }
     }
 
@@ -33,8 +30,29 @@ fn solve_a(input: &str) -> usize {
     input.lines().enumerate().filter(|(i, line)| verify_game(line)).map(|(i, _)| i + 1).sum()
 }
 
+fn calc_cube_power(line: &str) -> u32 {
+    let results = line.split(':').skip(1).next().unwrap();
+    let cubes = results.split([';', ',']).map(|cube| cube.trim());
+    let mut max_red = 0;
+    let mut max_green = 0;
+    let mut max_blue = 0;
+    for cube in cubes {
+        let mut split = cube.split(' ');
+        let count: u32 = split.next().unwrap().parse().ok().unwrap();
+        let color = split.next().unwrap();
+        match color {
+            "red" => max_red = max(max_red, count),
+            "green" => max_green = max(max_green, count),
+            "blue" => max_blue = max(max_blue, count),
+            _ => {}
+        }
+    }
+
+    max_red * max_green * max_blue
+}
+
 fn solve_b(input: &str) -> u32 {
-    0
+    input.lines().map(|line| calc_cube_power(line)).sum()
 }
 
 fn main() {
@@ -71,13 +89,13 @@ mod tests {
 
     #[test]
     fn example_a() {
-        let result = solve_a(include_str!("./example_a.txt"));
+        let result = solve_a(include_str!("./example.txt"));
         assert_eq!(result, 8);
     }
 
-    // #[test]
-    // fn example_b() {
-    //     let result = solve_b(include_str!("./example_b.txt"));
-    //     assert_eq!(result, 281);
-    // }
+    #[test]
+    fn example_b() {
+        let result = solve_b(include_str!("./example.txt"));
+        assert_eq!(result, 2286);
+    }
 }
