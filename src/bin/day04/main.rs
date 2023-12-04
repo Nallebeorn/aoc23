66d41545c646d2b1842455648658f88ac4a821c6
Benjamin <f.benjamin.larsson@gmail.com>
2023-12-04 20:15:38 +0100

    Day 04 part b

--- src/bin/day04/main.rs
+++ src/bin/day04/main.rs
@@ -1,12 +1,10 @@
 use std::{env, time::Instant};
 
-use itertools::Itertools;
-
 const DAY: u32 = 4;
 
 fn score_game(game: &str) -> usize {
     let mut split = game.split(':').skip(1).next().unwrap().split('|');
-    let winners = split.next().unwrap().split(' ').unique();
+    let winners = split.next().unwrap().split(' ');
     let have: Vec<&str> = split.next().unwrap().split(' ').collect();
 
     let num_winners = winners.filter(|num| !num.is_empty() && have.contains(num)).count();
@@ -17,8 +15,29 @@ fn solve_a(input: &str) -> usize {
     input.lines().map(|line| score_game(line)).sum()
 }
 
-fn solve_b(input: &str) -> u32 {
-    0
+fn score_card_b(game: &str) -> usize {
+    let mut split = game.split(':').skip(1).next().unwrap().split('|');
+    let winners = split.next().unwrap().split(' ');
+    let have: Vec<&str> = split.next().unwrap().split(' ').collect();
+
+    winners.filter(|num| !num.is_empty() && have.contains(num)).count()
+}
+
+fn solve_b(input: &str) -> usize {
+    let card_inputs: Vec<&str> = input.lines().collect();
+    let mut cards: Vec<usize> = (0..card_inputs.len()).collect();
+
+    let mut i = 0;
+    while i < cards.len() {
+        let card_number = cards[i];
+        let score = score_card_b(card_inputs[card_number]);
+        for card_won in card_number + 1..card_number + 1 + score {
+            cards.push(card_won);
+        }
+        i += 1;
+    }
+
+    cards.len()
 }
 
 fn main() {
@@ -59,9 +78,9 @@ mod tests {
         assert_eq!(result, 13);
     }
 
-    // #[test]
-    // fn example_b() {
-    //     let result = solve_b(include_str!("./example.txt"));
-    //     assert_eq!(result, 2286);
-    // }
+    #[test]
+    fn example_b() {
+        let result = solve_b(include_str!("./example.txt"));
+        assert_eq!(result, 30);
+    }
 }
