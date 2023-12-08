c160f9762e548c2d6199f0165bc6c5b5ede63dca
Benjamin <f.benjamin.larsson@gmail.com>
2023-12-08 20:12:48 +0100

    Day 08 part b

--- src/bin/day08/main.rs
+++ src/bin/day08/main.rs
@@ -1,4 +1,5 @@
-use std::{env, time::Instant, collections::HashMap};
+use std::{collections::HashMap, env, time::Instant};
+use num::integer::lcm;
 
 const DAY: u32 = 8;
 
@@ -11,7 +12,11 @@ fn solve_a(input: &str) -> usize {
     for node in nodes_input {
         let mut words = node.split_ascii_whitespace();
         let name = words.next().unwrap();
-        let left = words.nth(1).unwrap().trim_start_matches('(').trim_end_matches(',');
+        let left = words
+            .nth(1)
+            .unwrap()
+            .trim_start_matches('(')
+            .trim_end_matches(',');
         let right = words.next().unwrap().trim_end_matches(')');
         nodes.insert(name, (left, right));
     }
@@ -23,9 +28,9 @@ fn solve_a(input: &str) -> usize {
         let next_node = match direction {
             b'L' => curr_node.0,
             b'R' => curr_node.1,
-            _ => panic!()
+            _ => panic!(),
         };
-        
+
         num_steps += 1;
 
         if next_node == "ZZZ" {
@@ -37,7 +42,47 @@ fn solve_a(input: &str) -> usize {
 }
 
 fn solve_b(input: &str) -> usize {
-    0
+    let mut lines = input.lines();
+    let directions = lines.next().unwrap().bytes().cycle();
+
+    let nodes_input = lines.skip(1);
+    let mut nodes = HashMap::new();
+    for node in nodes_input {
+        let mut words = node.split_ascii_whitespace();
+        let name = words.next().unwrap();
+        let left = words
+            .nth(1)
+            .unwrap()
+            .trim_start_matches('(')
+            .trim_end_matches(',');
+        let right = words.next().unwrap().trim_end_matches(')');
+        nodes.insert(name, (left, right));
+    }
+
+    let starting_nodes = nodes.keys().filter(|key| key.ends_with('A'));
+    let cycle_step_counts = starting_nodes.map(|start| {
+        let mut directions = directions.clone();
+        let mut num_steps = 0;
+        let mut curr_node = nodes[start];
+        loop {
+            let direction = directions.next().unwrap();
+            let next_node = match direction {
+                b'L' => curr_node.0,
+                b'R' => curr_node.1,
+                _ => panic!(),
+            };
+
+            num_steps += 1;
+
+            if next_node.ends_with('Z') {
+                break num_steps;
+            }
+
+            curr_node = *nodes.get(next_node).unwrap();
+        }
+    });
+
+    cycle_step_counts.reduce(|a, b| lcm(a, b)).unwrap()
 }
 
 fn main() {
@@ -84,9 +129,9 @@ mod tests {
         assert_eq!(result, 6);
     }
 
-    // #[test]
-    // fn example_b() {
-    //     let result = solve_b(include_str!("./example.txt"));
-    //     assert_eq!(result, 71503);
-    // }
+    #[test]
+    fn example_b() {
+        let result = solve_b(include_str!("./example_b.txt"));
+        assert_eq!(result, 6);
+    }
 }
