33c510d88dfa1e6c3beec9afb7153f67aca03094
Benjamin <f.benjamin.larsson@gmail.com>
2023-12-12 00:17:37 +0100

    Fghklfjghlfjhlkfgjdhlkfgj

--- src/bin/day10/main.rs
+++ src/bin/day10/main.rs
@@ -73,7 +73,119 @@ fn solve_a(input: &str) -> i32 {
 }
 
 fn solve_b(input: &str) -> i32 {
-    0
+    let width = input.lines().next().unwrap().len();
+
+    let get_idx = |x: i32, y: i32| -> usize {
+        return (y * (width as i32) + x) as usize;
+    };
+
+    let height = input.lines().count();
+    let mut src_grid = vec!['.'; width * height];
+    let mut grid = vec![None; width * height];
+    let (mut startx, mut starty) = (0, 0);
+    for (y, line) in input.lines().enumerate() {
+        for (x, c) in line.bytes().enumerate() {
+            let (x, y) = (x as i32, y as i32);
+            src_grid[get_idx(x, y)] = c as char;
+            grid[get_idx(x, y)] = match c {
+                b'.' => None,
+                b'|' => Some((get_idx(x, y - 1), get_idx(x, y + 1))),
+                b'-' => Some((get_idx(x - 1, y), get_idx(x + 1, y))),
+                b'L' => Some((get_idx(x, y - 1), get_idx(x + 1, y))),
+                b'J' => Some((get_idx(x, y - 1), get_idx(x - 1, y))),
+                b'7' => Some((get_idx(x - 1, y), get_idx(x, y + 1))),
+                b'F' => Some((get_idx(x + 1, y), get_idx(x, y + 1))),
+                b'S' => {
+                    (startx, starty) = (x as i32, y as i32);
+                    None
+                }
+                _ => panic!(),
+            }
+        }
+    }
+
+    let possible_connections: [(i32, i32); 4] = [(0, -1), (1, 0), (0, 1), (-1, 0)];
+    let start_idx = get_idx(startx, starty);
+    let mut start_con_a = None;
+    let mut start_con_b = None;
+    for (ofsx, ofsy) in possible_connections {
+        let (x, y) = (startx + ofsx, starty + ofsy);
+        let idx = get_idx(x, y);
+        if idx < grid.len() {
+            if let Some((a, b)) = grid[idx] {
+                if a == start_idx || b == start_idx {
+                    if start_con_a.is_none() {
+                        start_con_a = Some(get_idx(x, y));
+                    } else {
+                        if get_idx(x, y) != start_con_a.unwrap() {
+                            start_con_b = Some(get_idx(x, y));
+                            break;
+                        }
+                    }
+                }
+            }
+        }
+    }
+
+    grid[start_idx] = Some((start_con_a.unwrap(), start_con_b.unwrap()));
+
+    let mut is_loop = vec![false; width * height];
+    let mut index = start_idx;
+    let mut prev = start_idx;
+    loop {
+        let (a, b) = grid[index].unwrap();
+        let next = if a == prev { b } else { a };
+        prev = index;
+        index = next;
+
+        is_loop[index] = true;
+
+        if index == start_idx {
+            break;
+        }
+    }
+
+    let mut num_inside = 0;
+    let mut debug = String::new();
+    for index in 0..is_loop.len() {
+        if index % width == 0 {
+            debug.push('\n');
+        }
+
+        if is_loop[index] {
+            debug.push(src_grid[index]);
+            continue;
+        }
+
+        let mut num_crossings = 0;
+        let mut check_idx = index as i32 - width as i32;
+        while check_idx > 0 {
+            if is_loop[check_idx as usize] {
+                if src_grid[check_idx as usize] != '|' {
+                    num_crossings += 1;
+                }
+                loop {
+                    let (a, b) = grid[check_idx as usize].unwrap();
+                    if !(a == check_idx as usize - width || b == check_idx as usize - width) {
+                        break;
+                    }
+                    check_idx -= width as i32;
+                }
+            }
+            check_idx -= width as i32;
+        }
+
+        if num_crossings % 2 == 1 {
+            num_inside += 1;
+            debug.push('I');
+        } else {
+            debug.push('.');
+        }
+    }
+
+    print!("{}", debug);
+
+    num_inside
 }
 
 fn main() {
@@ -110,13 +222,19 @@ mod tests {
 
     #[test]
     fn example_a() {
-        let result = solve_a(include_str!("./example.txt"));
+        let result = solve_a(include_str!("./example_a.txt"));
         assert_eq!(result, 4);
     }
 
-    // #[test]
-    // fn example_b() {
-    //     let result = solve_b(include_str!("./example.txt"));
-    //     assert_eq!(result, 2);
-    // }
+    #[test]
+    fn example_b() {
+        let result = solve_b(include_str!("./example_b_1.txt"));
+        assert_eq!(result, 4);
+    }
+
+    #[test]
+    fn example_b_2() {
+        let result = solve_b(include_str!("./example_b_2.txt"));
+        assert_eq!(result, 8);
+    }
 }
